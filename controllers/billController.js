179ef856c1633b707e957244f2c32f8a1bbac65f
jaimebp07 <andrescodigo07@gmail.com>
2022-09-10 23:48:13 -0500

    presentada

--- controllers/billController.js
+++ controllers/billController.js
@@ -2,24 +2,35 @@ const Bill = require('../model/bill');
 const Product = require('../model/product');
 
 module.exports.addBill = (req, res) => {
+    let json_bill = req.body.imput_bill_invisible;
     
-    let objBill = JSON.parse(req.body.imput_bill_invisible);
+    if(json_bill.length > 0){
+        let objBill = JSON.parse(json_bill);
+        let date = new Date().toISOString().split('T')[0];
+        let hour = new Date().toString().split(' ')[4];
 
-    const bill = new Bill({
-        bill_name_client: objBill.name_client,
-    bill_document_client: objBill.document_client,
-    bill_price_total: objBill.price_total,
-    bill_discount_rate: objBill.discount_rate,
-    bill_price_discount: objBill.price_discount,
-    bill_products: objBill.products_client
-    })
-    bill.save(function(error, bill){
-        if(error){
-            return res.status(500).json({
-                message: 'Error al crear el producto'
-            })
-        }
-       res.redirect('/make_sale');
-    })
+        console.log("objBill  "+ objBill)
+        const bill = new Bill({
+            bill_name_client: objBill.name_client,
+            bill_name_seller: objBill.name_client,
+            bill_date_purchase: date,
+            bill_hour_purchase: hour,
+            bill_document_client: objBill.document_client,
+            bill_price_total: objBill.price_total,
+            bill_discount_rate: objBill.discount_rate,
+            bill_price_discount: objBill.price_discount,
+            bill_products: objBill.products_client
+        })
+        bill.save(function(error, bill){
+            if(error){
+                return res.status(500).json({
+                    message: 'Error al crear el producto'
+                })
+            }
+           res.redirect('/make_sale');
+        })
+    } else {
+        console.log("FACTURA VACIA")
+    }
     
 }
--- model/bill.js
+++ model/bill.js
@@ -3,6 +3,9 @@ const Schema = mongoose.Schema
 
 const billSchema = new Schema ({
     bill_name_client: String,
+    bill_name_seller: String,
+    bill_date_purchase: String,
+    bill_hour_purchase: String,
     bill_document_client: String,
     bill_price_total: String,
     bill_discount_rate: String,
